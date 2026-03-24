066bf349938c724645fc17a64a8705759d0a4a05
Sunil Kumar <133530069+sunil22516@users.noreply.github.com>
2026-03-25 04:00:49 +0530

    Implement model download from Google Drive
    Added functionality to download the model from Google Drive if it does not exist locally.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from PIL import Image
 import io
 import os
 import tensorflow as tf
+import gdown
+import os
 
 app = FastAPI(title="ROCK-ing BOT API")
 
@@ -18,7 +20,16 @@ app.add_middleware(
 )
 
 # ── Load model once at startup ────────────────────────────────
+
 MODEL_PATH = "rock_paper_scissors_model.h5"
+FILE_ID = "https://drive.google.com/file/d/1GZ_G0dA269_UfnM3cmKdyrTI1uQlJlrS/view?usp=sharing"   # ← only thing you change
+
+# Download model if not already present
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", MODEL_PATH, quiet=False)
+    print("Model downloaded!")
+
 model = tf.keras.models.load_model(MODEL_PATH)
 
 # ── Label map — sorted alphabetically (matches your training) ─
